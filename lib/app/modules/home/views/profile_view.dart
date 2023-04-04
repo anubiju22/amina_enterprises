edba51d40262188a24c512cfc6b82ff48c7b64a0
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-04-04 16:20:46 +0530

    main

--- lib/app/modules/home/views/profile_view.dart
+++ lib/app/modules/home/views/profile_view.dart
@@ -11,166 +11,145 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CommonAppBar(
-        label: 'Profile',
-      ),
-      // body: Container(
-      //   decoration: const BoxDecoration(
-      //     color: scaffoldBgColor,
-      //   ),
-      //   child: ListView(
-      //     shrinkWrap: true,
-      //     physics: const ScrollPhysics(),
-      //     children: [
-      //       Container(
-      //         margin: const EdgeInsets.only(bottom: 10),
-      //         width: double.infinity,
-      //         decoration: const BoxDecoration(
-      //           image: DecorationImage(
-      //               image: AssetImage(
-      //                 "assets/image/profile_bg.png",
-      //               ),
-      //               fit: BoxFit.cover),
-      //         ),
-      //         child: Column(
-      //           crossAxisAlignment: CrossAxisAlignment.center,
-      //           mainAxisAlignment: MainAxisAlignment.center,
-      //           children: [
-      //             Row(
-      //               mainAxisAlignment: MainAxisAlignment.end,
-      //               children: [
-      //                 // visibility default false
-      //                 IconButton(
-      //                   onPressed: (() {}),
-      //                   icon: Visibility(
-      //                     visible: false,
-      //                     child: Image.asset(
-      //                       "assets/svg_icon/edit.svg",
-      //                       width: 25,
-      //                       height: 25,
-      //                     ),
-      //                   ),
-      //                 ),
-      //               ],
-      //             ),
-      //             // const DealerImgWidget(),
-      //             const SizedBox(
-      //               height: 10,
-      //             ),
-      //             Text("Anshad"),
-      //             const SizedBox(
-      //               height: 5,
-      //             ),
-      //             Text("C-KL-000001"),
-      //           ],
-      //         ),
-      //       ),
-      //       const EmployeeBottomWidget(),
-      //     ],
-      //   ),
-      // )
-    );
+        appBar: const CommonAppBar(
+          label: 'Profile',
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            color: scaffoldBgColor,
+          ),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(child: Image.asset("assets/image/profilecircle.png")),
+                  // const DealerImgWidget(),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  blackText("Anshad", 16, fontWeight: FontWeight.w600),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  blackText("C-KL-000001", 14, fontWeight: FontWeight.w500),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+              const EmployeeBottomWidget(),
+            ],
+          ),
+        ));
   }
 }
 
-// class EmployeeBottomWidget extends GetView<DashboardController> {
-//   const EmployeeBottomWidget({
-//     Key? key,
-//   }) : super(key: key);
+class EmployeeBottomWidget extends StatelessWidget {
+  const EmployeeBottomWidget({
+    Key? key,
+  }) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     var spacer = SizedBox(
-//       height: MediaQuery.of(context).size.height * .015,
-//     );
-//     return Container(
-//       //  height: MediaQuery.of(context).size.height * .7,
-//       width: double.infinity,
-//       decoration: const BoxDecoration(
-//           borderRadius: BorderRadius.only(
-//             topLeft: Radius.circular(50),
-//             topRight: Radius.circular(50),
-//           ),
-//           color: Colors.white),
-//       child: ListView(
-//         shrinkWrap: true,
-//         physics: const NeverScrollableScrollPhysics(),
-//         // mainAxisAlignment: MainAxisAlignment.start,
-//         // crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 //Header
-//                 spacer,
-//                 Text("Personal Details"),
-//                 spacer,
+  @override
+  Widget build(BuildContext context) {
+    var spacer = SizedBox(
+      height: MediaQuery.of(context).size.height * .015,
+    );
+    return Container(
+      //  height: MediaQuery.of(context).size.height * .7,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
+          ),
+          color: Colors.white),
+      child: ListView(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Header
+                spacer,
+                blackText("Personal Details", 18, fontWeight: FontWeight.w800),
+                spacer,
+                Row(
+                  children: [
+                    svgWidget("assets/svg/home_profile.svg",
+                        size: 20,
+                        color: const ColorFilter.mode(
+                            Colors.grey, BlendMode.srcIn)),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    greyText("Anshad", 16, fontWeight: FontWeight.w500),
+                    const Spacer(),
+                  ],
+                ),
+                // Email
+                spacer,
+                Row(
+                  children: [
+                    svgWidget('assets/svg/Call.svg', size: 15),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    greyText("+91 8563524047", 16, fontWeight: FontWeight.w500),
+                    const Spacer(),
 
-//                 spacer,
-//                 // Mobile number
-//                 Row(
-//                   children: [
-//                     svgWidget("assets/svg_icon/phone.svg"),
-//                     const SizedBox(
-//                       width: 10,
-//                     ),
-//                     Text("Anshad!"),
-//                     const Spacer(),
-//                   ],
-//                 ),
-//                 // Email
-//                 spacer,
-//                 Row(
-//                   children: [
-//                     svgWidget("assets/svg_icon/phone.svg"),
-//                     const SizedBox(
-//                       width: 10,
-//                     ),
-//                     Text("Email"),
-//                     const Spacer(),
+                    // const Spacer(),
+                    // IconWithOnTapWidget(
+                    //   icon: "assets/svg_icon/edit.svg",
+                    //   onTap: () {
+                    //     //controller.logout();
+                    //   },
+                    // ),
+                  ],
+                ),
 
-//                     // const Spacer(),
-//                     // IconWithOnTapWidget(
-//                     //   icon: "assets/svg_icon/edit.svg",
-//                     //   onTap: () {
-//                     //     //controller.logout();
-//                     //   },
-//                     // ),
-//                   ],
-//                 ),
+                spacer,
+                Row(
+                  children: [
+                    svgWidget('assets/svg/email.svg', size: 15),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    greyText("anshad000@gmail.com", 16,
+                        fontWeight: FontWeight.w500),
+                    const Spacer(),
+                  ],
+                ),
+                spacer,
+                // Divider with color
+                devider(context),
+                // Profile settings
+                spacer,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-//                 spacer,
-//                 Row(
-//                   children: [
-//                     svgWidget("assets/svg_icon/phone.svg"),
-//                     const SizedBox(
-//                       width: 10,
-//                     ),
-//                     Text("Anshad!"),
-//                     const Spacer(),
-//                   ],
-//                 ),
-//                 spacer,
-//                 // Divider with color
-//                 devider(context),
-//                 // Profile settings
-//                 spacer,
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Container devider(BuildContext context) {
-//     return Container(
-//       width: double.infinity,
-//       height: MediaQuery.of(context).size.height * .005,
-//       color: Colors.grey.withOpacity(.2),
-//     );
-//   }
-// }
+  Container devider(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * .005,
+      color: Colors.grey.withOpacity(.2),
+    );
+  }
+}
