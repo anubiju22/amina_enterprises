0e22d9f06b834ddaea134b006c9af8efb8bd8439
anubiju22 <anuit16@gmail.com>
2023-04-05 14:03:38 +0530

    main

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -31,122 +31,128 @@ class HomeView extends GetView<HomeController> {
         ),
         backgroundColor: scaffoldBgColor,
         body: SingleChildScrollView(
-          child:
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        greyText('Hi, Anshad', 14),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        blackText('Welcome', 20, fontWeight: FontWeight.w700),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.23,
-                          height: MediaQuery.of(context).size.height * 0.04,
-                          child: CommonButtonWidget(
-                            label: 'LOGIN',
-                            borderRadius: 15,
-                            fontSize: 12,
-                            onClick: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => CameraView(
-                                          camera:
-                                              controller.cameras[direction])));
-                            },
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          greyText('Hi, Anshad', 14),
+                          const SizedBox(
+                            height: 10,
                           ),
-                        )
-                      ],
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.15,
-                      height: MediaQuery.of(context).size.height * 0.10,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Color(0xFFE7E7E7)),
-                      child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Image.asset('assets/image/profile_mask.png')),
-                    )
-                  ],
+                          blackText('Welcome', 20, fontWeight: FontWeight.w700),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.23,
+                            height: MediaQuery.of(context).size.height * 0.04,
+                            child: CommonButtonWidget(
+                              label: 'LOGIN',
+                              borderRadius: 15,
+                              fontSize: 12,
+                              onClick: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => CameraView(
+                                            camera: controller
+                                                .cameras[direction])));
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.15,
+                        height: MediaQuery.of(context).size.height * 0.10,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Color(0xFFE7E7E7)),
+                        child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child:
+                                Image.asset('assets/image/profile_mask.png')),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            blackText(
-              'Menu',
-              22,
-              fontWeight: FontWeight.w700,
-            ),
-            sizedBox,
-            GridView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4, mainAxisSpacing: 20.0
+              const SizedBox(
+                height: 15,
+              ),
+              blackText(
+                'Menu',
+                22,
+                fontWeight: FontWeight.w700,
+              ),
+              sizedBox,
+              GridView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4, mainAxisSpacing: 20.0
 
-                    // number of columns in the grid
-                    ),
-                itemCount: controller.menuItems.length,
-                itemBuilder: (context, index) {
-                  return HomeCardItem(
-                    path: controller.menuItems[index].image,
-                    label: controller.menuItems[index].label,
-                    ontap: controller.menuItems[index].onClick,
-                  );
-                }),
-            const SizedBox(
-              height: 20,
-            ),
-            blackText('Categories', 22, fontWeight: FontWeight.w700),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                HomeCategories(
-                  path: 'assets/svg/home_footwear.svg',
-                  label: 'Footwears',
-                  color: const ColorFilter.mode(homeFootColor, BlendMode.srcIn),
-                  onClick: () {
-                    Get.toNamed(Routes.FOOTWEARS);
-                  },
-                ),
-                HomeCategories(
-                  path: 'assets/svg/home_bag.svg',
-                  label: 'Bags',
-                  color: const ColorFilter.mode(homeBagColor, BlendMode.srcIn),
-                  onClick: () {},
-                ),
-                HomeCategories(
-                  path: 'assets/svg/home_jacket.svg',
-                  label: 'Apparels',
-                  color: const ColorFilter.mode(homeAppColor, BlendMode.srcIn),
-                  onClick: () {},
-                )
-              ],
-            )
-          ]),
+                      // number of columns in the grid
+                      ),
+                  itemCount: controller.menuItems.length,
+                  itemBuilder: (context, index) {
+                    return HomeCardItem(
+                      path: controller.menuItems[index].image,
+                      label: controller.menuItems[index].label,
+                      ontap: controller.menuItems[index].onClick,
+                    );
+                  }),
+              const SizedBox(
+                height: 20,
+              ),
+              blackText('Categories', 22, fontWeight: FontWeight.w700),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  HomeCategories(
+                    path: 'assets/svg/home_footwear.svg',
+                    label: 'Footwears',
+                    color:
+                        const ColorFilter.mode(homeFootColor, BlendMode.srcIn),
+                    onClick: () {
+                      Get.toNamed(Routes.FOOTWEARS);
+                    },
+                  ),
+                  HomeCategories(
+                    path: 'assets/svg/home_bag.svg',
+                    label: 'Bags',
+                    color:
+                        const ColorFilter.mode(homeBagColor, BlendMode.srcIn),
+                    onClick: () {},
+                  ),
+                  HomeCategories(
+                    path: 'assets/svg/home_jacket.svg',
+                    label: 'Apparels',
+                    color:
+                        const ColorFilter.mode(homeAppColor, BlendMode.srcIn),
+                    onClick: () {},
+                  )
+                ],
+              )
+            ]),
+          ),
         ));
   }
 }
