a9faefb25c8e26eb3d25929bed20863eecadafe4
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-04-05 10:23:03 +0530

    main

--- lib/app/modules/myroute/views/calender.dart
+++ lib/app/modules/myroute/views/calender.dart
@@ -14,31 +14,27 @@ class HorizontalCalendar extends StatelessWidget {
         itemCount: 30,
         itemBuilder: (BuildContext context, int index) {
           final DateTime date = DateTime.now().add(Duration(days: index));
-          Color colors = scaffoldBgColor;
-          if (date == DateTime.now) {
-            Color colors = primaryColor;
-          } else {
-            Color colors = scaffoldBgColor;
-          }
-          return _buildDay(date, colors);
+
+          return _buildDay(date);
         },
       ),
     );
   }
 
-  Widget _buildDay(DateTime date, Color? colors) {
+  Widget _buildDay(
+    DateTime date,
+  ) {
     final formatter = DateFormat('E');
     final dayOfWeek = formatter.format(date);
 
     return Container(
       width: 50,
-      margin: EdgeInsets.all(5),
-      decoration:
-          BoxDecoration(borderRadius: BorderRadius.circular(8.0), color: colors
-
-              // Colors.grey[300],
-              // scaffoldBgColor,
-              ),
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: DateTime.now().day == date.day ? primaryColor : scaffoldBgColor,
+        // scaffoldBgColor,
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
