18717ffd41963d0e38d481d9b2597d82d2d30c53
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-04-06 15:47:11 +0530

    main

--- lib/app/common_widgets/card/shop_card.dart
+++ lib/app/common_widgets/card/shop_card.dart
@@ -23,74 +23,77 @@ class ShopCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 115,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.white,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BlackText(
-              text: shopname,
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Row(
-              children: [
-                svgWidget('assets/svg/location.svg'),
-                const SizedBox(
-                  width: 5,
-                ),
-                GreyText(
-                  text: location,
-                )
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Row(
-              children: [
-                svgWidget('assets/svg/Call.svg', size: 15),
-                const SizedBox(
-                  width: 5,
-                ),
-                GreyText(
-                  text: number.toString(),
-                )
-              ],
-            ),
-            // Visibility(
-            //   visible: visibles!,
-            //   child: Row(
-            //     children: [
-            //       svgWidget('assets/svg/time.svg', size: 15),
-            //       const SizedBox(
-            //         width: 5,
-            //       ),
-            //       GreyText(
-            //         text: time.toString(),
-            //       ),
-            //       const SizedBox(
-            //         width: 50,
-            //       ),
-            //       svgWidget('assets/svg/timer.svg', size: 15),
-            //       const SizedBox(
-            //         width: 5,
-            //       ),
-            //       GreyText(
-            //         text: timer.toString(),
-            //       )
-            //     ],
-            //   ),
-            // ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Container(
+        // height: 115,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BlackText(
+                text: shopname,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  svgWidget('assets/svg/location.svg'),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GreyText(
+                    text: location,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  svgWidget('assets/svg/Call.svg', size: 15),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GreyText(
+                    text: number.toString(),
+                  )
+                ],
+              ),
+              // Visibility(
+              //   visible: visibles!,
+              //   child: Row(
+              //     children: [
+              //       svgWidget('assets/svg/time.svg', size: 15),
+              //       const SizedBox(
+              //         width: 5,
+              //       ),
+              //       GreyText(
+              //         text: time.toString(),
+              //       ),
+              //       const SizedBox(
+              //         width: 50,
+              //       ),
+              //       svgWidget('assets/svg/timer.svg', size: 15),
+              //       const SizedBox(
+              //         width: 5,
+              //       ),
+              //       GreyText(
+              //         text: timer.toString(),
+              //       )
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
