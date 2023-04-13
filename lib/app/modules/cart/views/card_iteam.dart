8e70cb6bc7721ab25a36993c8713e31f10f5c54b
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-04-13 14:22:47 +0530

    main

--- lib/app/modules/cart/views/card_iteam.dart
+++ lib/app/modules/cart/views/card_iteam.dart
@@ -1,17 +1,15 @@
+import 'package:amina_enterprises/app/common_widgets/button/loginbutton.dart';
+import 'package:amina_enterprises/app/common_widgets/svg_icons/svg_widget.dart';
+import 'package:amina_enterprises/app/common_widgets/texts/login_text.dart';
+import 'package:amina_enterprises/app/common_widgets/texts/text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../common_widgets/textfeild/numbertextfeild.dart';
+
 class CartItemCard extends StatelessWidget {
-  // final CartResponse cartResponse;
-  // final int index;
-  // final Function removeClick;
-  // final TextEditingController totalQuantityController;
   const CartItemCard({
     Key? key,
-    // required this.cartResponse,
-    // required this.removeClick,
-    // required this.index
-    // required this.totalQuantityController,
   }) : super(key: key);
 
   @override
@@ -19,7 +17,8 @@ class CartItemCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.min,
         children: [
           Row(
@@ -28,7 +27,6 @@ class CartItemCard extends StatelessWidget {
             children: [
               Image.network(
                 "https://icon2.cleanpng.com/20180323/eaq/kisspng-slipper-footwear-shoe-sandal-flip-flops-men-shoes-5ab57871171a07.8676672615218422890946.jpg",
-                // cartResponse.img,
                 width: MediaQuery.of(context).size.width * .3,
               ),
               Flexible(
@@ -42,84 +40,51 @@ class CartItemCard extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            " cartResponse.artNo,",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              blackText(" GP4300", 18,
+                                  fontWeight: FontWeight.w500),
+                              greyText("Gents Covering", 12,
+                                  fontWeight: FontWeight.w400),
+                            ],
                           ),
                           const SizedBox(
                             width: 5,
                           ),
-                          Text(
-                            // "(${cartResponse.categoryName})",
-                            "hi",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.normal,
-                                color: Colors.grey,
-                                fontSize: 12),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
                           IconButton(
-                              onPressed: () {
-                                //  removeClick();
-                              },
-                              icon: const Icon(Icons.delete_outline))
+                              onPressed: () {},
+                              icon:
+                                  svgWidget("assets/svg/delete.svg", size: 19))
                         ],
+                      ),
+                      const SizedBox(
+                        height: 20,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
+                        children: const [
                           Text(
-                            " cartResponse.brandName,",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.normal,
-                                color: Colors.grey,
-                                fontSize: 13),
-                          ),
-                          Text(
-                            "cartResponse.productType.toUpperCase(),",
+                            "₹ 289",
                             style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                color: Colors.red.shade300,
-                                fontSize: 12),
+                              color: Color(0xffd80005),
+                              fontSize: 18,
+                              fontFamily: "Manrope",
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "CARTON",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Color(0xff2f2f2f),
+                              fontSize: 12,
+                            ),
                           ),
                         ],
                       ),
                       const SizedBox(
                         height: 5,
-                      ),
-                      Text(
-                        "cartResponse.productName,",
-                        style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey,
-                            fontSize: 13),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        //   mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "mrp",
-                            // "₹ ${cartResponse.mrp}",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            "   cartResponse.orderType,",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.grey),
-                          ),
-                        ],
                       ),
                     ],
                   ),
@@ -130,55 +95,107 @@ class CartItemCard extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          // SizedBox(
-          //   width: double.infinity,
-          //   child: Wrap(
-          //       spacing: 10,
-          //       runSpacing: 10,
-          //       children: cartResponse.itemSize
-          //           .map((size) => SizeItemWidget(
-          //                 value: size.size,
-          //                 qty: size.qty,
-          //                 addPress: () {
-          //                   if (cartResponse.orderType == 'Cut size') return;
-          //                   controller.updateCartQty(
-          //                       cartResponse, size, "Add");
-          //                 },
-          //                 lessPress: () {
-          //                   if (cartResponse.orderType == 'Cut size') return;
-          //                   if (size.qty < 2) return;
-          //                   controller.updateCartQty(
-          //                       cartResponse, size, "Less");
-          //                 },
-          //               ))
-          //           .toList()),
-          // ),
-          // if (cartResponse.orderType == 'Cut size')
-          //   Container(
-          //     padding: const EdgeInsets.all(10),
-          //     child: Row(
-          //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //       children: [
-          //         const Text(
-          //           "Enter Required Quantity",
-          //           style: TextStyle(fontSize: 12),
-          //         ),
-          //         SizedBox(
-          //           width: 55,
-          //           height: 35,
-          //           child: CommonTextField(
-          //             // textEditingController: totalQuantityController,
-          //             hintText: cartResponse.totalQuantity,
-          //             isCenterText: true,
-          //             onChanged: (dynamic value) {
-          //               controller.updateCutSizeRequiredQuntity(index, value);
-          //             },
-          //           ),
-          //         )
-          //       ],
-          //     ),
-          //   )
+          Row(
+            children: const [
+              Text(
+                "5*8",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff393939),
+                  fontSize: 17,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              NumberTextfeild(),
+              SizedBox(
+                width: 15,
+              ),
+              DeleteButton(),
+              SizedBox(
+                width: 50,
+              ),
+              Text(
+                "7*8",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff393939),
+                  fontSize: 17,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              NumberTextfeild(),
+              SizedBox(
+                width: 15,
+              ),
+              DeleteButton(),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: const [
+              Text(
+                "6*8",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff393939),
+                  fontSize: 17,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              NumberTextfeild(),
+              SizedBox(
+                width: 15,
+              ),
+              DeleteButton(),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          const Divider(
+            thickness: 5,
+            color: Color(0xFFF3F3F3),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class DeleteButton extends StatelessWidget {
+  const DeleteButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 32,
+      width: 32,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        border: Border.all(
+          color: Colors.grey,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: SizedBox(
+        child: IconButton(
+            onPressed: () {},
+            icon: svgWidget("assets/svg/delete.svg", size: 15)),
       ),
     );
   }
--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -1,4 +1,6 @@
 import 'package:amina_enterprises/app/common_widgets/app_bar/common_app_bar.dart';
+import 'package:amina_enterprises/app/common_widgets/button/loginbutton.dart';
+import 'package:amina_enterprises/app/common_widgets/texts/text.dart';
 import 'package:amina_enterprises/app/modules/cart/views/card_iteam.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +13,55 @@ class CartView extends GetView<CartController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CommonAppBar(
+      appBar: CommonAppBar(
         label: "Cart",
         visibility: false,
       ),
       body: CartItemCard(),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(15),
+        child: CommonButtonWidget(
+          label: "CHECKOUT",
+          onClick: () {
+            Get.back();
+          },
+        ),
+      ),
     );
+  }
+}
+
+class BottomWidgets extends StatelessWidget {
+  const BottomWidgets({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          blackText("Total Amount", 18, fontWeight: FontWeight.w400),
+          Text(
+            "₹5252 ",
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              color: Color(0xFFD80005),
+              fontSize: 18,
+            ),
+          )
+        ],
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      CommonButtonWidget(
+        label: "CHECKOUT",
+        onClick: () {
+          Get.back();
+        },
+      ),
+    ]);
   }
 }
