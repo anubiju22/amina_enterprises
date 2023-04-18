59f25d7bb23d1ae03bd9fb7206a522359232c8d9
anubiju22 <anuit16@gmail.com>
2023-04-18 17:46:30 +0530

    main

--- lib/app/modules/login/views/otp_view.dart
+++ lib/app/modules/login/views/otp_view.dart
@@ -27,7 +27,6 @@ class OtpView extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  flex: 4,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -37,77 +36,158 @@ class OtpView extends StatelessWidget {
                     ],
                   ),
                 ),
-                Expanded(
-                    flex: 3,
+                SingleChildScrollView(
                     child: Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(32),
-                          topRight: Radius.circular(32),
-                        ),
-                        color: scaffoldBgColor,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            const LoginText(
-                              text: "Enter OTP",
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            OtpTextfield(
-                              oncomplete: (pin) {
-                                return null;
-                              },
-                              onchange: (pin) {
-                                // controller.otp = pin;
-                                return null;
-                                // return null;
-                              },
-                            ),
-                            TextButton(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32),
+                    ),
+                    color: scaffoldBgColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          const LoginText(
+                            text: "Enter OTP",
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03,
+                          ),
+                          OtpTextfield(
+                            oncomplete: (pin) {
+                              return null;
+                            },
+                            onchange: (pin) {
+                              // controller.otp = pin;
+                              return null;
+                              // return null;
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Didn’t receive OTP?",
+                                style: TextStyle(color: Color(0xFF818181)),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              TextButton(
+                                style: ButtonStyle(
+                                  padding: MaterialStateProperty.all<
+                                      EdgeInsetsGeometry>(
+                                    EdgeInsets.zero,
+                                  ),
+                                ),
                                 onPressed: () {},
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: const [
-                                    Text(
-                                      "Didn’t receive OTP?",
-                                      style:
-                                          TextStyle(color: Color(0xFF818181)),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      "Resend",
-                                      style: TextStyle(color: redColor),
-                                    )
-                                  ],
-                                )),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            CommonButtonWidget(
-                              label: "VERIFY OTP",
-                              onClick: () {
-                                Get.toNamed(Routes.HOME);
-                              },
-                            )
+                                child: const Text(
+                                  "Resend",
+                                  style: TextStyle(color: redColor),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03,
+                          ),
+                          CommonButtonWidget(
+                            label: "VERIFY OTP",
+                            onClick: () {
+                              Get.toNamed(Routes.HOME);
+                            },
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.025,
+                          ),
 
-                            // )
-                          ],
-                        ),
+                          // )
+                        ],
                       ),
-                    ))
+                    ),
+                  ),
+                ))
               ],
             ),
           ),
         ));
   }
 }
+
+// Expanded(
+//                     flex: 3,
+//                     child: Container(
+//                       decoration: const BoxDecoration(
+//                         borderRadius: BorderRadius.only(
+//                           topLeft: Radius.circular(32),
+//                           topRight: Radius.circular(32),
+//                         ),
+//                         color: scaffoldBgColor,
+//                       ),
+//                       child: Padding(
+//                         padding: const EdgeInsets.symmetric(horizontal: 20),
+//                         child: Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: [
+//                             const SizedBox(
+//                               height: 30,
+//                             ),
+//                             const LoginText(
+//                               text: "Enter OTP",
+//                             ),
+//                             const SizedBox(
+//                               height: 20,
+//                             ),
+//                             OtpTextfield(
+//                               oncomplete: (pin) {
+//                                 return null;
+//                               },
+//                               onchange: (pin) {
+//                                 // controller.otp = pin;
+//                                 return null;
+//                                 // return null;
+//                               },
+//                             ),
+//                             TextButton(
+//                                 onPressed: () {},
+//                                 child: Row(
+//                                   mainAxisAlignment: MainAxisAlignment.center,
+//                                   children: const [
+//                                     Text(
+//                                       "Didn’t receive OTP?",
+//                                       style:
+//                                           TextStyle(color: Color(0xFF818181)),
+//                                     ),
+//                                     SizedBox(
+//                                       width: 5,
+//                                     ),
+//                                     Text(
+//                                       "Resend",
+//                                       style: TextStyle(color: redColor),
+//                                     )
+//                                   ],
+//                                 )),
+//                             const SizedBox(
+//                               height: 20,
+//                             ),
+//                             CommonButtonWidget(
+//                               label: "VERIFY OTP",
+//                               onClick: () {
+//                                 Get.toNamed(Routes.HOME);
+//                               },
+//                             )
+
+//                             // )
+//                           ],
+//                         ),
+//                       ),
+//                     ))
