ba235b8f0c4bf3a69b9a5e6f158437fc92d6dc4a
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-04-20 16:06:36 +0530

    main

--- lib/app/modules/pending_collection/views/pending_collection_view.dart
+++ lib/app/modules/pending_collection/views/pending_collection_view.dart
@@ -1,4 +1,5 @@
 import 'package:amina_enterprises/app/common_widgets/app_bar/common_app_bar.dart';
+import 'package:amina_enterprises/app/common_widgets/button/date_button.dart';
 import 'package:amina_enterprises/app/common_widgets/card/view_payment_card.dart';
 import 'package:amina_enterprises/app/common_widgets/textfeild/search_feild.dart';
 import 'package:amina_enterprises/constraints/app_colors.dart';
@@ -14,63 +15,82 @@ class PendingCollectionView extends GetView<PendingCollectionController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CommonAppBar(label: "Pending Collection"),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CommonSearchTextField(
-                    hintText: 'SELECT SHOP',
-                    isEnabled: true,
-                    autofocus: false,
-                    readOnly: true,
-                    // textEditingController:
-                    //     shopPaymentState.selectShopController,
-                    onPressed: () {},
-                    onTap: () async {},
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DateButton(
+                    size: MediaQuery.of(context).size,
+                    ontapp: () {},
+                    icons: Icons.calendar_month,
+                    label: "From Date"),
+                DateButton(
+                    size: MediaQuery.of(context).size,
+                    ontapp: () {},
+                    icons: Icons.calendar_month,
+                    label: "To Date")
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CommonSearchTextField(
+                      hintText: 'SELECT SHOP',
+                      isEnabled: true,
+                      autofocus: false,
+                      readOnly: true,
+                      // textEditingController:
+                      //     shopPaymentState.selectShopController,
+                      onPressed: () {},
+                      onTap: () async {},
+                    ),
                   ),
                 ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                    width: 60,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        shape: BoxShape.rectangle,
-                        gradient: primaryColor),
-                    child: Icon(
-                      Icons.filter_alt,
-                      color: Colors.white,
-                      size: 25,
-                    )),
-              ),
-            ],
-          ),
-          Expanded(
-              child: ListView.builder(
-            physics: ScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: 2,
-            itemBuilder: ((context, index) {
-              return ViewPaymentCard(
-                amount: "  ₹ Amount: 10000",
-                amtVisible: true,
-                balance: "Balance: ₹3000",
-                date: "21 Apr 2021",
-                // dueVisible: true,
-                invamount: "Order Amount: ₹13000",
-                invdate: "Order Date: 21 Apr 2022",
-                location: "Crystal Building, Malad, Rathodi, Mankavu, Calicut",
-                shop: "PRINCE FOOTWEAR BANDBAHAL",
-                visible: true,
-              );
-            }),
-          ))
-        ],
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                      width: 60,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          shape: BoxShape.rectangle,
+                          gradient: primaryColor),
+                      child: Icon(
+                        Icons.filter_alt,
+                        color: Colors.white,
+                        size: 25,
+                      )),
+                ),
+              ],
+            ),
+            Expanded(
+                child: ListView.builder(
+              physics: ScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: ((context, index) {
+                return ViewPaymentCard(
+                  amount: "  ₹ Amount: 10000",
+                  amtVisible: true,
+                  balance: "Balance: ₹3000",
+                  date: "21 Apr 2021",
+                  // dueVisible: true,
+                  invamount: "Order Amount: ₹13000",
+                  invdate: "Order Date: 21 Apr 2022",
+                  location:
+                      "Crystal Building, Malad, Rathodi, Mankavu, Calicut",
+                  shop: "PRINCE FOOTWEAR BANDBAHAL",
+                  visible: true,
+                );
+              }),
+            ))
+          ],
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
