1623bfe3cd11402e4ad55ea566b2e9672b8edece
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2023-09-06 17:14:07 +0530

    main

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 
 import '../controllers/splash_controller.dart';
@@ -10,18 +9,11 @@ class SplashView extends GetView<SplashController> {
   Widget build(BuildContext context) {
     Get.put(SplashController());
     return Scaffold(
-        body: AnimationConfiguration.staggeredList(
-      position: 0,
-      child: FadeInAnimation(
-        duration: const Duration(milliseconds: 800),
-        // verticalOffset: 50.0,
-        child: Image.asset(
-          'assets/image/splash_kvn.png',
-          fit: BoxFit.fill,
-          width: double.infinity,
-          height: double.infinity,
-        ),
-      ),
+        body: Image.asset(
+      'assets/image/splash_kvn.png',
+      fit: BoxFit.fill,
+      width: double.infinity,
+      height: double.infinity,
     ));
   }
 }
